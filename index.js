bc2aa56cc4afa125d6499f4fc10c207b5b506348
Marco <marco.kosters@hva.nl>
2023-03-11 18:45:05 +0100

    basically klaar
    hele website af denk ik tenminste het grootste gedeelte maybe nog wat details doen als ik zin heb

--- index.js
+++ index.js
@@ -21,15 +21,27 @@ app.get('/', function (req, res) {
 })
 
 app.get('/beschrijving', (request, response) => {
-  response.render('beschrijving')
+  response.render('beschrijving', data)
 })
 
 app.get('/stappenplan', (request, response) => {
-  response.render('stappenplan')
+  response.render('stappenplan', data)
+})
+
+app.get('/stappenplan2', (request, response) => {
+  response.render('stappenplan2', data)
+})
+
+app.get('/stappenplan3', (request, response) => {
+  response.render('stappenplan3', data)
+})
+
+app.get('/stappenplan4', (request, response) => {
+  response.render('stappenplan4', data)
 })
 
 app.get('/voorbeelden', (request, response) => {
-  response.render('voorbeelden')
+  response.render('voorbeelden', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
